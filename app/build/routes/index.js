1df466b954b7623e924191c089208fe6f2f86888
martucazpo <martucazpo@gmail.com>
2023-05-26 13:15:07 -0700

    bundle app

--- app/build/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const authorRoutes = require("./authors")
-const apiRoutes = require("./api")
-
-router.use("/freecodecamp/authors", authorRoutes)
-router.use("/jsonplaceholder/api", apiRoutes)
-router.route("/").get((req, res)=>res.sendFile(process.cwd() + "/public/index.html"))
-
-module.exports = router
